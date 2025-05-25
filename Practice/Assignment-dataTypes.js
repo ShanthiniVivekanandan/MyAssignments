efbca5406856aac9df813e043b76ff24a612fb4a
ShanthiniVivekanandan <shanthini233@gmail.com>
2025-05-25 10:57:01 +0530

    Add files via upload
    Day1

---
+++ Practice/Assignment-dataTypes.js
@@ -0,0 +1,20 @@
+let firstName="Shanthini";
+console.log(firstName);
+console.log("Datatype of firstName is "+ typeof firstName);
+let companyName=null;
+console.log(companyName);
+console.log("Datatype of companytName is "+ typeof companyName); 
+let mobileNumber=95667888900;
+console.log(mobileNumber);
+console.log("Datatype of MobileNumber is "+typeof mobileNumber);
+let isAutomation=true;
+console.log(isAutomation);
+console.log("Datatype of isAutomation is "+typeof isAutomation);
+let hasPlaywright;
+console.log(hasPlaywright);
+console.log("Datatype of isAutomation is "+typeof hasPlaywright);
+
+
+
+
+
---
+++ Practice/datatypes.js
@@ -0,0 +1,39 @@
+//numbers
+
+var phoneNumber=898989;
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+var phoneNumber="898989";
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+var population =8656966526554543666999990098880000000n;
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+//string
+var userName="shanthini";
+var password='&123456';
+console.log(userName);
+console.log(password);
+
+console.log("Datatype of username is " + typeof userName);
+console.log("datatype of password is " + typeof password)
+
+//boolean
+var accnumvalid= true;
+console.log(typeof accnumvalid);
+
+//undefined
+var accountNumber; 
+console.log(accountNumber);
+console.log(typeof accountNumber);
+
+//null
+var phoneNumber=null;
+console.log(phoneNumber);
+console.log(typeof phoneNumber);//object-historical bug
+
+
+
